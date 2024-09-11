52173ebffa11c761bee061d19c954f17ed81a697
Amanda <amhu0002@stud.kea.dk>
2024-09-11 14:25:05 +0200

    udfylder html og js

---
+++ javascript/singleview.js
@@ -0,0 +1,18 @@
+//const urlParams = new URLSearchParams(window.location.search);
+//const id = urlParams.get("id");
+
+//fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+//.then((res) => res.json())
+//.then((data) => showProduct(data));
+
+//function showProduct(product) {
+//document.querySelector("h3").textContent = product.productdisplayname;
+//document.querySelector(".brand").textContent = product.brandname;
+//document.querySelector(".model").textContent = product.variantname;
+//document.querySelector(".color").textContent = product.colour1;
+//document.querySelector(".id-number").textContent = product.id;
+//document.querySelector(".brandbio").textContent = product.brandbio;
+//document.querySelector(
+//"img"
+//).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+//}
