6a920b30a9717c430a37bdc0bad65224bdd69896
AmandaTougaard <amto0001@stud.kea.dk>
2024-09-12 13:29:21 +0200

    Kommentar

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,7 @@
 //https://wbceqwohnppgnysnvrai.supabase.co
-
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Vxd29obnBwZ255c252cmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU5NTIsImV4cCI6MjA0MTU0MTk1Mn0.Pd99gTQP-qyB1L2B6oiqnON_HDXMovH-FgbFuDLpscc
+
+//henter URL fra supabase
 
 fetch("https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/products?brand=eq.Canon", {
   method: "GET",
@@ -11,6 +12,10 @@ fetch("https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/products?brand=eq.Canon"
   .then((res) => res.json())
   .then(showData);
 
+//Viser data
+
 function showData(items) {
   console.log(items);
 }
+
+//Data ud i console
