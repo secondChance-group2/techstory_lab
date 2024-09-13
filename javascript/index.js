2de44be5c8236c84e74e61426657c1b19db74544
Amanda <amhu0002@stud.kea.dk>
2024-09-13 09:07:20 +0200

    kommentar

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,18 @@
 //https://wbceqwohnppgnysnvrai.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Vxd29obnBwZ255c252cmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU5NTIsImV4cCI6MjA0MTU0MTk1Mn0.Pd99gTQP-qyB1L2B6oiqnON_HDXMovH-FgbFuDLpscc
 
-//henter URL fra supabase
+//henter URL'en med data fra supabase
 
-fetch("https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/products?brand=eq.Canon", {
-  method: "GET",
-  headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Vxd29obnBwZ255c252cmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU5NTIsImV4cCI6MjA0MTU0MTk1Mn0.Pd99gTQP-qyB1L2B6oiqnON_HDXMovH-FgbFuDLpscc",
-  },
-})
+fetch(
+  "https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/products?brand=eq.Canon",
+  {
+    method: "GET",
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Vxd29obnBwZ255c252cmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU5NTIsImV4cCI6MjA0MTU0MTk1Mn0.Pd99gTQP-qyB1L2B6oiqnON_HDXMovH-FgbFuDLpscc",
+    },
+  }
+)
   .then((res) => res.json())
   .then(showData);
 
