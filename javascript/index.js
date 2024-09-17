fa13aade6906d045e6e45e9f887f4ab0dac7597e
Amanda <amhu0002@stud.kea.dk>
2024-09-17 15:26:27 +0200

    new categories

--- javascript/index.js
+++ javascript/index.js
@@ -3,23 +3,33 @@
 
 //henter URL'en med data fra supabase
 
-fetch(
-  "https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/products?brand=eq.Canon",
-  {
-    method: "GET",
-    headers: {
-      apikey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Vxd29obnBwZ255c252cmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU5NTIsImV4cCI6MjA0MTU0MTk1Mn0.Pd99gTQP-qyB1L2B6oiqnON_HDXMovH-FgbFuDLpscc",
-    },
-  }
-)
+const url = "https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/tsl";
+const key =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Vxd29obnBwZ255c252cmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU5NTIsImV4cCI6MjA0MTU0MTk1Mn0.Pd99gTQP-qyB1L2B6oiqnON_HDXMovH-FgbFuDLpscc";
+
+const options = {
+  headers: {
+    apikey: key,
+  },
+};
+
+fetch(url, options)
   .then((res) => res.json())
-  .then(showData);
+  .then(show);
+
+function show(data) {
+  console.log(data);
+}
 
 //Viser data
 
-function showData(items) {
-  console.log(items);
-}
+// function showCategory(cat) {
+//   const template = document.querySelector("template").content;
 
-//Data ud i console
+//   const clone = template.cloneNode(true);
+
+//   clone.querySelector("a").textContent = cat.category;
+//   clone.querySelector("a").href = `equipment.html?category${cat.category}`;
+
+//   document.querySelector("ol").appendChild(clone);
+// }
