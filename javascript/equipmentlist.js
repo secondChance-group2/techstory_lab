2193bb5eed3c94a39e7770d3904d2f5efb1f9e09
Amanda <amhu0002@stud.kea.dk>
2024-09-18 13:06:09 +0200

    equipmentlist virker

--- javascript/equipmentlist.js
+++ javascript/equipmentlist.js
@@ -7,16 +7,25 @@ let main;
 let template;
 
 function init() {
-  main = document.querySelector("main");
+  main = document.querySelector("#equipment_container");
   template = document.querySelector("#equipmentTemplate").content;
   if (params.has("category")) {
     productsURI = `https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/tsl?category=${category}`;
   } else {
     productsURI = `https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/tsl?limit=10`;
   }
-  fetch(productsURI)
+  fetch(
+    `https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/tsl?limit=5&category=eq.${category}`,
+    {
+      method: "GET",
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Vxd29obnBwZ255c252cmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU5NTIsImV4cCI6MjA0MTU0MTk1Mn0.Pd99gTQP-qyB1L2B6oiqnON_HDXMovH-FgbFuDLpscc",
+      },
+    }
+  )
     .then((res) => res.json())
-    .then(handleData);
+    .then((data) => handleData(data));
 }
 
 function handleData(json) {
@@ -29,8 +38,11 @@ function showProduct(product) {
   const copy = template.cloneNode(true);
   //ændre indhold
 
-  copy.querySelector("h3").textContent = tsl.category;
+  document.querySelector("h1").textContent = product.category;
 
+  copy.querySelector(".brand2").textContent = product.brand;
+  copy.querySelector(".product_name").textContent = product.product_name_model;
+  copy.querySelector(".type2").textContent = product.type;
   //appende
   main.appendChild(copy);
 }
