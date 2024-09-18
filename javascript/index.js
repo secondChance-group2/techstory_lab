8e9cae4014d3fc6aa9d915d209989be25a193f24
Amanda <amhu0002@stud.kea.dk>
2024-09-18 10:26:49 +0200

    tutor hjælp

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,8 @@
 
 //henter URL'en med data fra supabase
 
-const url = "https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/tsl";
+const url =
+  "https://wbceqwohnppgnysnvrai.supabase.co/rest/v1/tsl?select=category";
 const key =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Vxd29obnBwZ255c252cmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU5NTIsImV4cCI6MjA0MTU0MTk1Mn0.Pd99gTQP-qyB1L2B6oiqnON_HDXMovH-FgbFuDLpscc";
 
@@ -19,6 +20,8 @@ fetch(url, options)
 
 function showCategories(cats) {
   console.log(cats);
+
+  // we should filter categories here, to only show one
   cats.forEach(showCategory);
 }
 
