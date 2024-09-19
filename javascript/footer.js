4a7479ffdad2f53f2bff502a7c8baffc8a68bbdd
Anarikke <anla0008@stud.kea.dk>
2024-09-19 08:36:12 +0200

    styling

---
+++ javascript/footer.js
@@ -0,0 +1,5 @@
+// function saAutomatedLink(element, type) {
+//   // Add tracking functionality here (if required)
+//   console.log("Outbound link clicked:", element.href);
+//   return true; // Ensure link works after tracking
+// }
